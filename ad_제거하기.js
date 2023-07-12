6eb376c49f2da44b2402c51f12eb080be3a5c0b6
sabb12 <sabb0923@gamil.com>
2023-07-12 22:35:32 +0900

    2023.07.12

---
+++ ad_제거하기.js
@@ -0,0 +1,20 @@
+
+let strArr = ["and","notad","abcd"];
+
+function solution(strArr) {
+    var answer = [];
+    
+    // for(let i = 0; i < strArr.length; i++){
+    //     console.log("str[i]",strArr[i].includes("ad"))
+    //     if(!strArr[i].includes("ad")){
+    //         answer.push(strArr[i])
+    //     }
+    // }
+      // return answer;
+       strArr = strArr.filter((x)=>{
+        return x.indexOf("ad") == -1
+    })
+    return strArr;
+}
+
+solution(strArr)
