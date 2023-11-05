9645fef2b3b9ee08358e2bb943b76e96b9dcad9c
sabb12 <sabb0923@gamil.com>
2023-11-05 22:02:20 +0900

    최솟값 만들기_Lv2

---
+++ 최솟값 만들기_Lv2.js
@@ -0,0 +1,18 @@
+// function solution(A,B){
+//    let result = 0;
+//      A.sort((a,b) => a - b)
+//      B.sort((a,b) => b - a)
+
+//     for(let i = 0; i < A.length; i++){  
+//        result +=  A[i] * B[i]
+//     }
+//     return result
+// }
+
+function solution(A,B){
+    A.sort((a,b) => a - b)
+    B.sort((a,b) => b - a)
+    let result = A.reduce((acc,cur,i) => (acc + (cur * B[i])),0)
+
+  return result
+}
