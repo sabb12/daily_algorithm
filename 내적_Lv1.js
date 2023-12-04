b861c165e2d3939e74c091175c23b86aeab3089e
sabb12 <sabb0923@gamil.com>
2023-12-04 14:47:11 +0900

    핸드폰번호가리기, 가운데글자가져오기, 내적  풀이

---
+++ 내적_Lv1.js
@@ -0,0 +1,13 @@
+// function solution(a, b) {
+//     let answer = 0;
+    
+//     for(let i = 0; i < a.length; i++){
+//         answer += a[i] * b[i]
+//     }
+    
+//     return answer;
+// }
+
+function solution(a, b) {
+    return a.reduce((acc, cur, i) =>  acc + cur * b[i] ,0);
+}
