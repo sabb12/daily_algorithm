b913a536263eff920c24ba6d9fed3bf3eb0f3b22
sabb12 <sabb0923@gamil.com>
2023-12-07 15:02:46 +0900

    약수의개수와덧셈 수정, 부족한금액계산하기 및 문자열 내림차순으로배치하기 풀기

---
+++ 부족한_금액_계산하기_Lv1.js
@@ -0,0 +1,9 @@
+function solution(price, money, count) {
+    var answer = 0;
+    
+    for(let i = 1; i <= count; i++){
+        answer += i * price
+    }
+
+    return money > answer ? 0 : answer - money
+}
--- 약수의_개수와_덧셈_Lv1.js
+++ 약수의_개수와_덧셈_Lv1.js
@@ -6,3 +6,23 @@ function solution(left, right) {
     }      
     return answer;
 }
+
+// function solution(left, right) {
+//     let answer = 0;
+
+//     for(let i = left; i <= right; i++){
+//         let array = [];
+//         for(let k = 1; k <= i; k++){
+//            // console.log(i, k,i % k === 0)
+//             if(i % k == 0){
+//                 array.push(k)
+//             }
+//         } 
+//         if(array.length % 2 === 0){
+//             answer += i
+//         }else{
+//             answer -= i
+//         }
+//     }      
+//     return answer;
+// }
