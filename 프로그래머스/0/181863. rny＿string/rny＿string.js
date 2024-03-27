c4d39c1ab17ca39af5dcf7e335ad946fdf5342b5
Leo <sabb12@hotmail.com>
2024-03-27 12:09:24 +0900

    [level 0] Title: rny_string, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181863. rny＿string/rny＿string.js
+++ 프로그래머스/0/181863. rny＿string/rny＿string.js
@@ -1,11 +1,12 @@
 function solution(rny_string) {
-    var answer = "";
-    for(let i = 0; i < rny_string.length; i++){
-        if(rny_string[i] === "m"){
-            answer += "rn"
-        }else{
-           answer += rny_string[i]
-        }
-    }
-    return answer;
+    // var answer = "";
+    // for(let i = 0; i < rny_string.length; i++){
+    //     if(rny_string[i] === "m"){
+    //         answer += "rn"
+    //     }else{
+    //        answer += rny_string[i]
+    //     }
+    // }
+    // return answer;
+    return rny_string.replaceAll(/m/g, "rn")
 }
