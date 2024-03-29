0415eb36d7a943f4a0bad94c5f6f0ffebbc4b558
Leo <sabb12@hotmail.com>
2024-03-29 15:59:08 +0900

    [level 0] Title: 배열의 유사도, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.js
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.js
@@ -1,11 +1,12 @@
 function solution(s1, s2) {
-    let result = 0;
-    for(let i = 0; i < s2.length; i++){
-       for(let k = 0; k < s1.length; k++){
-           if(s2[i] === s1[k]){
-               result++
-           }
-       }
-    }
-    return result
+    // let result = 0;
+    // for(let i = 0; i < s2.length; i++){
+    //    for(let k = 0; k < s1.length; k++){
+    //        if(s2[i] === s1[k]){
+    //            result++
+    //        }
+    //    }
+    // }
+    // return result
+    return s2.filter(s => s1.includes(s)).length
 }
