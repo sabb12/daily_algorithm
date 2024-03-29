7af9fd4b73c655a7875bc22d339f51056c8bdf48
Leo <sabb12@hotmail.com>
2024-03-29 16:00:13 +0900

    [level 0] Title: 배열의 평균값, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.js
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.js
@@ -1,7 +1,8 @@
 function solution(numbers) {
-     let sum = 0;
-    for(let i = 0; i < numbers.length; i++){
-        sum += numbers[i]
-    }
-    return sum / numbers.length
+    //  let sum = 0;
+    // for(let i = 0; i < numbers.length; i++){
+    //     sum += numbers[i]
+    // }
+    // return sum / numbers.length
+return numbers.reduce((acc, cur)=> (acc + cur)) / numbers.length
 }
