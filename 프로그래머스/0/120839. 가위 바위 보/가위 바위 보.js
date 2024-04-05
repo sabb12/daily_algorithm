fb3d42eabc5a6864c4a071831b6f3400e07acfea
Leo <sabb12@hotmail.com>
2024-04-05 16:09:35 +0900

    [level 0] Title: 가위 바위 보, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.js
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.js
@@ -1,14 +1,22 @@
 function solution(rsp) {
-    var answer = '';
-    for(let i = 0; i < rsp.length; i++){
-        if(rsp[i] === "2"){
-            answer += "0"
-        }else if(rsp[i] === "0"){
-            answer += "5";
-        }else if(rsp[i] === "5"){
-            answer += "2"
+//     var answer = '';
+//     for(let i = 0; i < rsp.length; i++){
+//         if(rsp[i] === "2"){
+//             answer += "0"
+//         }else if(rsp[i] === "0"){
+//             answer += "5";
+//         }else if(rsp[i] === "5"){
+//             answer += "2"
 
-        }
+//         }
+//     }
+//     return answer;
+   
+    const object = {
+        2: 0,
+        0: 5,
+        5: 2,
     }
-    return answer;
+    return [...rsp].map(i => object[i]).join("")
+    
 }
