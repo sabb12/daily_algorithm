896374cb4c7931c063351dc11ded55c49b90cfc7
Leo <sabb12@hotmail.com>
2024-04-05 19:28:55 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.js
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.js
@@ -1,6 +1,22 @@
 function solution(array) {
 
+        for (let i = 0; i < array.length - 1; i++) {
+    for (let j = 0; j < array.length - 1 - i; j++) {
+        // 현재 요소와 다음 요소 비교하여 정렬
+        if (array[j] > array[j + 1]) {
+            // 현재 요소가 다음 요소보다 크면 위치 교환
+            const temp = array[j];
+            array[j] = array[j + 1];
+            array[j + 1] = temp;
+        }
+    }
+}
+
+return array[(array.length / 2) - 0.5]; 
     
-    return array[Math.floor(array.sort((a,b)=> a - b).length / 2)]
+    
+    
+    // return array[Math.floor(array.sort((a,b)=> a - b).length / 2)]
+
 
 }
