d0ec39246f9deee467cce63cac8159b08743ec9d
Leo <sabb12@hotmail.com>
2024-04-09 15:02:29 +0900

    [level 0] Title: 각도기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120829. 각도기/각도기.js
+++ 프로그래머스/0/120829. 각도기/각도기.js
@@ -1,24 +1,14 @@
 function solution(angle) {
 
-    if(angle < 90){
-        return 1
-    }else if(angle === 90){
-        return 2
-    }else if(angle < 180){
-        return 3
-    }else if(angle === 180){
-        return 4
-    }
-    
-    // if(angle === 180){
-    //     return 4
-    // }else if(angle <= 180){
-    //     return 3
+    // if(angle < 90){
+    //     return 1
     // }else if(angle === 90){
     //     return 2
-    // }else if(angle <= 90){
-    //     return 1
+    // }else if(angle < 180){
+    //     return 3
+    // }else if(angle === 180){
+    //     return 4
     // }
-  
-    
+
+    return [0,90,91,180].filter(num => angle >= num).length
 }
