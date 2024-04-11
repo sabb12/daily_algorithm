5bb1262da089cdd9b9bc725110ff2d93e70329a9
Leo <sabb12@hotmail.com>
2024-04-11 15:32:57 +0900

    [level 0] Title: 모음 제거, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.js
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.js
@@ -1,21 +1,22 @@
 function solution(my_string) {
     
-    let result = "";
-    for(let i = 0; i < my_string.length; i++){
-        if(my_string[i] === "a"){
-            result +=""
-        }else if(my_string[i] === "e"){
-             result += ""
-        }else if(my_string[i] === "i"){
-             result += ""
-        }else if(my_string[i] === "o"){
-             result += ""
-        }else if(my_string[i] === "u"){
-            result += ""
-        }else{
-            result += my_string[i]
-        }
-    }
+//     let result = "";
+//     for(let i = 0; i < my_string.length; i++){
+//         if(my_string[i] === "a"){
+//             result +=""
+//         }else if(my_string[i] === "e"){
+//              result += ""
+//         }else if(my_string[i] === "i"){
+//              result += ""
+//         }else if(my_string[i] === "o"){
+//              result += ""
+//         }else if(my_string[i] === "u"){
+//             result += ""
+//         }else{
+//             result += my_string[i]
+//         }
+//     }
     
-    return result;
+//     return result;
+    return my_string.replace(/[aeiou]/g, "")
 }
