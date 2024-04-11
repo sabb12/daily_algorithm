698db12b2f485434dd7a85dec63ece63f04afd83
Leo <sabb12@hotmail.com>
2024-04-11 15:30:40 +0900

    [level 0] Title: 모음 제거, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.js
@@ -0,0 +1,21 @@
+function solution(my_string) {
+    
+    let result = "";
+    for(let i = 0; i < my_string.length; i++){
+        if(my_string[i] === "a"){
+            result +=""
+        }else if(my_string[i] === "e"){
+             result += ""
+        }else if(my_string[i] === "i"){
+             result += ""
+        }else if(my_string[i] === "o"){
+             result += ""
+        }else if(my_string[i] === "u"){
+            result += ""
+        }else{
+            result += my_string[i]
+        }
+    }
+    
+    return result;
+}
