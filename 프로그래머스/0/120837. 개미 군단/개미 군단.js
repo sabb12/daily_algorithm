c253a67b3e0212f5cba46d260c6c01f85b766d2f
Leo <sabb12@hotmail.com>
2024-04-12 10:13:32 +0900

    [level 0] Title: 개미 군단, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120837. 개미 군단/개미 군단.js
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.js
@@ -1,17 +1,18 @@
 function solution(hp) {
-    let result = 0;
+//     let result = 0;
 
-    while(hp > 0){
-        if(Math.floor(hp / 5)){
-            result += 1
-            hp -= 5
-        }else if(Math.floor(hp / 3)){
-            result += 1
-            hp -= 3
-        }else{
-            result += 1
-            hp -= 1
-        }
-    }
-    return result;
+//     while(hp > 0){
+//         if(Math.floor(hp / 5)){
+//             result += 1
+//             hp -= 5
+//         }else if(Math.floor(hp / 3)){
+//             result += 1
+//             hp -= 3
+//         }else{
+//             result += 1
+//             hp -= 1
+//         }
+//     }
+//     return result;
+    return Math.floor(hp/5)+Math.floor((hp%5)/3)+(hp%5)%3;
 }
