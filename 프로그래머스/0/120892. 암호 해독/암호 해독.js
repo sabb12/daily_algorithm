8970719272e18e2602ef0732d672076cdf21ce27
Leo <sabb12@hotmail.com>
2024-04-15 15:31:23 +0900

    [level 0] Title: 암호 해독, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120892. 암호 해독/암호 해독.js
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.js
@@ -1,7 +1,8 @@
 function solution(cipher, code) {
-    var answer = '';
-    for(let i = code -1; i < cipher.length; i+=code){
-        answer += cipher[i]
-    }
-    return answer;
+    // var answer = '';
+    // for(let i = code -1; i < cipher.length; i+=code){
+    //     answer += cipher[i]
+    // }
+    // return answer;
+    return [...cipher].filter((_,idx)=> (idx + 1) % code === 0).join("")
 }
