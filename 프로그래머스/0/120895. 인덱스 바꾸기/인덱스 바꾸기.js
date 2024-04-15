8b90f5e694b268e67ad10b29b763b8002c44b82d
Leo <sabb12@hotmail.com>
2024-04-15 15:19:14 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -5,9 +5,9 @@ function solution(my_string, num1, num2) {
     
     for(let i = 0; i < my_string.length; i++){
         if(i === num1){
-            answer += idx2
+            answer += my_string[num2]
         }else if(i === num2){
-            answer += idx1
+            answer += my_string[num1]
         }else{
             answer += my_string[i]
         }
