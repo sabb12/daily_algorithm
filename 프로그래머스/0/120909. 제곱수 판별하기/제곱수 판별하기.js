e7411b6e47eba02413316ffb8451840195f601a3
Leo <sabb12@hotmail.com>
2024-04-16 12:39:08 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.js
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.js
@@ -1,4 +1,4 @@
 function solution(n) {
-    var answer = 0;
-    return Math.sqrt(n) % 1 === 0 ? 1 : 2;
+    // return Math.sqrt(n) % 1 === 0 ? 1 : 2;
+    return Number.isInteger(Math.sqrt(n)) ? 1 : 2;
 }
