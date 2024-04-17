b95b3124c81363217f501ff4e14142303b52f321
Leo <sabb12@hotmail.com>
2024-04-17 16:25:41 +0900

    [level 1] Title: 약수의 합, Time: 0.53 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.js
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.js
@@ -1,11 +1,13 @@
 function solution(n) {
     var answer = 0;
     
-    for(let i = 1; i <= n; i++){
-        if(n % i === 0){
-            answer += i
-        }
-    }
+    // for(let i = 1; i <= n; i++){
+    //     if(n % i === 0){
+    //         answer += i
+    //     }
+    // }
     
-    return answer;
+    // return answer;
+    
+    return Array.from(Array(n), (_, index) => index + 1).reduce((acc,cur) => { return n % cur === 0 ? acc + cur : acc},0)
 }
