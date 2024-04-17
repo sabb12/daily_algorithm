323c4be48a8f67813c26ba79245faa560d553650
Leo <sabb12@hotmail.com>
2024-04-17 16:30:53 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.js
@@ -1,11 +1,12 @@
 function solution(n){
     
-    let answer = String(n);
-    let result = 0;
+//     let answer = String(n);
+//     let result = 0;
     
-    for(let i = 0; i < answer.length; i++){
-        result +=  Number(answer[i])
-    }
-
-    return result;
+//     for(let i = 0; i < answer.length; i++){
+//         result +=  Number(answer[i])
+//     }
+//     return result;
+    
+    return [...String(n)].reduce((acc,cur)=> acc + Number(cur),0)
 }
