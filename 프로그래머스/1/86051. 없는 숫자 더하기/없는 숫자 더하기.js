edc09ddf4e745ad45f7dc6f7c77c82ea111ac78e
Leo <sabb12@hotmail.com>
2024-04-19 19:10:29 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,9 +1,10 @@
 function solution(numbers) {
-    let answer = 0;
-    for(let i = 0; i < 10; i++){
-      if(!numbers.includes(i)){
-          answer += i
-      }
+    let answer = numbers.sort((a,b)=> a - b);
+    let sum = 0;
+    for(let i = 0; i <= 9; i++){
+      console.log(answer[i] !== i || answer[i] === undefined)
+           
+        
     }
     
     return answer;
