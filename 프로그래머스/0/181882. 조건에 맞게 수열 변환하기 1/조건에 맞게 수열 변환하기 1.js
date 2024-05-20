de9bf3db0a5afd19a11dace268afae12b207b8ab
Leo <sabb12@hotmail.com>
2024-05-20 11:38:50 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 1, Time: 3.65 ms, Memory: 44.2 MB -BaekjoonHub

--- 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.js
+++ 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.js
@@ -3,16 +3,16 @@ function solution(arr) {
     
     for(let i = 0; i < arr.length; i++){
         if(arr[i] >= 50 && arr[i] % 2 === 0){
-            answer.push(arr[i] / 2)
+            arr[i] /= 2
         }else if(arr[i] <= 50 && arr[i] % 2 === 1){
-            answer.push(arr[i] * 2)
+            arr[i] *= 2
         }else{
-            answer.push(arr[i])
+           arr[i] = arr[i]
         }
         
     }
     
-    return answer;
+    return  arr;
 }
 
 // if(arr[i] > 50 || arr[i] % 2 === 0)
