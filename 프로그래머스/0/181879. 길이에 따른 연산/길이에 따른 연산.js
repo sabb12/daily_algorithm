bbf16a1cefbc09295816b43ea37506bfa3308f11
Leo <sabb12@hotmail.com>
2024-05-20 11:17:48 +0900

    [level 0] Title: 길이에 따른 연산, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.js
@@ -0,0 +1,9 @@
+function solution(num_list) {
+    if(num_list.length > 10){
+        num_list = num_list.reduce((acc, cur) => acc + cur,0)
+    }else{
+        num_list = num_list.reduce((acc, cur) => acc * cur,1) 
+    }
+
+    return num_list
+}
