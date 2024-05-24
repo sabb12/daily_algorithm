476d5091a73b63b66783b706d44e2406ae76ac92
Leo <sabb12@hotmail.com>
2024-05-24 12:09:33 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.js
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.js
@@ -1,29 +1,31 @@
 function solution(age) {
-    let answer = '';
-    age = age.toString()
-    for(let i = 0; i < age.length; i++){
-       if(age[i] === "0"){
-           answer += "a";
-       }else if(age[i] === "1"){
-           answer += "b";
-       }else if(age[i] === "2"){
-           answer += "c";
-       }else if(age[i] === "3"){
-           answer += "d";
-       }else if(age[i] === "4"){
-           answer += "e";
-       }else if(age[i] === "5"){
-           answer += "f";
-       }else if(age[i] === "6"){
-           answer += "g";
-       }else if(age[i] === "7"){
-           answer += "h";
-       }else if(age[i] === "8"){
-           answer += "i";
-       }else if(age[i] === "9"){
-           answer += "j";
-       }
-    }
+    // let answer = '';
+    // age = age.toString()
+    // for(let i = 0; i < age.length; i++){
+    //    if(age[i] === "0"){
+    //        answer += "a";
+    //    }else if(age[i] === "1"){
+    //        answer += "b";
+    //    }else if(age[i] === "2"){
+    //        answer += "c";
+    //    }else if(age[i] === "3"){
+    //        answer += "d";
+    //    }else if(age[i] === "4"){
+    //        answer += "e";
+    //    }else if(age[i] === "5"){
+    //        answer += "f";
+    //    }else if(age[i] === "6"){
+    //        answer += "g";
+    //    }else if(age[i] === "7"){
+    //        answer += "h";
+    //    }else if(age[i] === "8"){
+    //        answer += "i";
+    //    }else if(age[i] === "9"){
+    //        answer += "j";
+    //    }
+    // }
     
-    return answer;
+    // return answer;
+    
+    return age.toString().split("").map(num => "abcdefghij"[num]).join("")
 }
