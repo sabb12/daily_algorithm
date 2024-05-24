a3dd1c9fd07346831a7a9eb062fa01c55055098f
Leo <sabb12@hotmail.com>
2024-05-24 10:32:39 +0900

    [level 0] Title: l로 만들기, Time: 7.39 ms, Memory: 39.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.js
@@ -0,0 +1,14 @@
+function solution(myString) {
+    let answer = '';
+    
+    for(let i = 0; i < myString.length; i++){
+        if(myString[i] < "l"){
+            answer += "l"
+        }else{
+            answer += myString[i]
+        }
+         
+    }
+    
+    return answer;
+}
