634677a310050ac71c58354b7defff9925dff5b6
Leo <sabb12@hotmail.com>
2025-05-06 11:50:26 +0900

    [level 1] Title: 예산, Time: 0.16 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.js
+++ 프로그래머스/1/12982. 예산/예산.js
@@ -1,16 +1,23 @@
 function solution(d, budget) {
+    // 방법 1
+    // let answer = 0;
+    // for(let i = 0; i < d.sort((a, b) => a - b).length; i++){
+    //   if(budget >= d[i]){
+    //      budget -= d[i]
+    //      answer++;
+    //   }else{
+    //      break;   
+    //   }
+    // }
+    // return answer;
+    // 방법 2
     
-    let answer = 0;
-    for(let i = 0; i < d.sort((a, b) => a - b).length; i++){
-      if(budget - Number(d[i]) > 0){
-         budget -= Number(d[i])
-         answer ++
-      }else if(budget - Number(d[i]) === 0){
-         budget -= Number(d[i])
-         answer++
-      }
+    let count = 0;
+     d.sort((a, b) => a - b).map((price) => {
+    if (budget >= price) {
+      budget -= price;
+      count++;
     }
-    
-    return answer;
-    
+  });
+    return count
 }
